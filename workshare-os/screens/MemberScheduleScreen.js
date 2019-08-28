ab5752fe146e03568c5b9674bcfef8f321e20b51
thejoyenjoy <joey.rpt@gmail.com>
2019-08-28 16:49:14 +0900

    Delete unnecessary code
    Delete unnecessar code and imports.

--- workshare-os/screens/MemberScheduleScreen.js
+++ workshare-os/screens/MemberScheduleScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const MemberScheduleScreen = ({navigation}) => {
+const MemberScheduleScreen = () => {
 
   return (
     <View style={styles.container}>
