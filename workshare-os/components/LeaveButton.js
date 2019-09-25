735a61017dc83e91b3127d4ff6f99dd0dadf76f3
thejoyenjoy <joey.rpt@gmail.com>
2019-09-25 12:37:26 +0900

    Extract updateUserStatus to auth component
    I outsourced the update user status to the auth component with just a type as props for testing.

--- workshare-os/components/LeaveButton.js
+++ workshare-os/components/LeaveButton.js
@@ -10,28 +10,29 @@ const LeaveButton = () => {
   const [isLeavePressed, setLeavePressed] = useState(false);
   const [userDocumentId, setUserDocumentId] = useState('');
 
-  useDidUpdateEffect(() => {
-    auth.db.collection('users').doc(userDocumentId).update({
-      'status.type': (isLeavePressed) ? 'yes' : 'no'
-    })
-  }, [userDocumentId]);
+  // useDidUpdateEffect(() => {
+  //   auth.db.collection('users').doc(userDocumentId).update({
+  //     'status.type': (isLeavePressed) ? 'yes' : 'no'
+  //   })
+  // }, [userDocumentId]);
 
   const auth = useAuth();
 
   const handleLeavePress = () => {
     setLeavePressed(!isLeavePressed);
+    auth.updateUsersStatus((isLeavePressed) ? 'yes' : 'no')
 
-    auth.db.collection('users')
-      .where('userId', '==', auth.user.uid)
-      .get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          setUserDocumentId(doc.id);
-        });
-      })
-      .catch(error => {
-        console.log('Error getting documents', error);
-      });
+    // auth.db.collection('users')
+    //   .where('userId', '==', auth.user.uid)
+    //   .get()
+    //   .then(snapshot => {
+    //     snapshot.forEach(doc => {
+    //       setUserDocumentId(doc.id);
+    //     });
+    //   })
+    //   .catch(error => {
+    //     console.log('Error getting documents', error);
+    //   });
   };
 
   return (
--- workshare-os/customHook/useAuth.js
+++ workshare-os/customHook/useAuth.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext, createContext } from "react";
+import useDidUpdateEffect from './useDidUpdateEffect';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import 'firebase/firestore';
@@ -33,6 +34,23 @@ export const useAuth = () => {
 function useProvideAuth() {
 
   const [user, setUser] = useState(null);
+
+  const [usersStatus, setUsersStatus] = useState({
+    documentId: '',
+    eventId: '',
+    from: 0,
+    to: 0,
+    statusType: '',
+  });
+
+  useDidUpdateEffect(() => {
+    db.collection('users').doc(usersStatus.documentId).update({
+      'status.eventId': usersStatus.eventId,
+      'status.from' : usersStatus.from,
+      'status.to' : usersStatus.to,
+      'status.type': usersStatus.statusType,
+    })
+  }, [usersStatus]);
 
   const signIn = (email, password) => {
     return firebase
@@ -81,6 +99,26 @@ function useProvideAuth() {
       });
   };
 
+  const updateUsersStatus = (type) =>{
+    db.collection('users')
+    .where('userId', '==', user.uid)
+    .get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        setUsersStatus({
+          documentId: doc.id,
+          eventId: '',
+          from: 0,
+          to: 0,
+          statusType: type,
+        });
+      });
+    })
+    .catch(error => {
+      console.log('Error getting documents', error);
+    });
+  }
+
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged(user => {
       if (user) {
@@ -99,6 +137,7 @@ function useProvideAuth() {
     signUp,
     signOut,
     sendPasswordResetEmail,
-    confirmPasswordReset
+    confirmPasswordReset,
+    updateUsersStatus,
   };
 }
